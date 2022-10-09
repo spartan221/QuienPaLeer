e9feff5652ffe9fd3268e5a9ad9232807f476121
Luis Mendez <luismzm.developer@gmail.com>
2022-10-09 18:59:33 -0500

    Creación apartados Equipo QPL y Contacto en el sidebar
    - Agregado Modal Equipo QPL
    - Agregado boton que lleva a un correo del proyecto
    - Modificado el apartado de usuario a un dropmenu

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import '../css/SideBar.css'
 import 'bootstrap/dist/css/bootstrap.css';
 import { NavLink } from 'react-router-dom';
 import { Button, Nav, NavItem } from 'react-bootstrap';
-import '../css/SideBar.css'
 import LogoQPLBlack from "../assets/img/QPL_Logo_Black.png";
 import TeamJson from '../assets/team.json';
 
 export default function SideBar() {
 
     let activeStyle = {
-        backgroundColor : '#ff9f43',
+        backgroundColor : '#ffb875',
         borderRadius: '5px',
     }
 
@@ -61,35 +61,55 @@ export default function SideBar() {
 
                 <div className='d-flex justify-content-evenly align-items-center'>
                     <div>
-                        <button type="button" className="btnExtrasSideBar btn" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        <button type="button" className="btn" id='btnInfoTeamSideBar' data-bs-toggle="modal" data-bs-target="#teamModal">
                             <i class="bi bi-info-lg"></i>
                         </button>
                     </div>
                     <div>
-                        <button type="button" className="btnExtrasSideBar btn" data-bs-toggle="modal" data-bs-target="#exampleModal" disabled >
-                            <i className="bi bi-envelope fs-5"></i>
-                        </button>
+                        <a href='mailto:ProyectoQPL.App@gmail.com'>
+                            <button type="button" className="btn text-black" id='btnInfoTeamSideBar'>
+                                <i class="bi bi-envelope">
+                                </i>
+                            </button>
+                        </a>
                     </div>
                 </div>
                 <br/>
                 <div className='text-center textAppVersionSideBar text-muted lead h6 mt-3'>
-                    App Version: <span>1.0</span>
+                    App Version: <span>2.4</span>
                 </div>
 
                 <hr/>
 
             </div>
 
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered">
-                    <div class="modal-content">
+            <div className="modal fade modal-lg" id="teamModal" tabindex="-1" aria-labelledby="teamModalLabel" aria-hidden="true">
+                <div className="modal-dialog modal-dialog-centered">
+                    <div className="modal-content">
                         <div className="modal-header d-flex justify-content-start">
-                            <h5 className='modal-title fw-bold' id="exampleModalLabel">Equipo</h5>
+                            <h5 className='modal-title fw-bold ms-2' id="teamModalLabel">Equipo</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
-                        <div class="modal-body">
-                            
+                        <div class="modal-body d-flex flex-row flex-wrap justify-content-center">
+                                {TeamJson.map((member, i) => (
+                                    <div className="card w-25 m-2 fw-lighter" key={i}>
+                                        <div className="card-header h-50">
+                                            <h6 className='text-center'>{member.firstname + ' ' + member.lastname}</h6>
+                                        </div>
+                                        <div className='card-body'>
+                                            <h6 className='text-center text-muted'>
+                                                {member.dev}
+                                            </h6>
+                                            <div className='d-flex justify-content-center'>
+                                                <a href={member.email} className='text-decoration-none'>
+                                                    <button className='btnContactMember btn mt-3'style={{fontSize: 10, backgroundColor: '#ffb875', color: 'black'}}>Contactar</button>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))}
                         </div>
-                        <div class="modal-footer">
+                        <div className="modal-footer">
                             <span><img src={LogoQPLBlack} id='qplLogoNavBar' className='' /></span>
                         </div>
                     </div>
