3a2ca3bba0bbe99d21cebf905ec6d65a80ff91c4
dabonilla <dabonilla@unal.edu.co>
2022-11-19 18:39:53 -0500

    Agregada validación en form de evento

--- frontend/src/components/ValidationField.jsx
+++ frontend/src/components/ValidationField.jsx
@@ -51,6 +51,22 @@ function validateEvent(name, file, description, startDate, endDate, hour, place)
   if (!description || description === '') newErrors.description = 'Ingresa una descripción.'
   if (!startDate || startDate === '') newErrors.startDate = 'Fecha inválida.'
   if (!endDate || endDate === '') newErrors.endDate = 'Fecha inválida.'
+  if (startDate && startDate != '' && startDate.length === 10 && endDate && endDate != '' && endDate.length === 10) {
+    if (parseInt(startDate.substring(0, 4)) <= parseInt(endDate.substring(0, 4))) {
+        if (parseInt(startDate.substring(5, 7)) <= parseInt(endDate.substring(5, 7))) {
+            if (parseInt(startDate.substring(8)) > parseInt(endDate.substring(8))) {
+                newErrors.startDate = "Fecha inválida."
+                newErrors.endDate = "Fecha inválida."
+            }
+        } else {
+            newErrors.startDate = "Fecha inválida."
+            newErrors.endDate = "Fecha inválida."
+        }
+    } else {
+        newErrors.startDate = "Fecha inválida."
+        newErrors.endDate = "Fecha inválida."
+    }
+  }
   if (!hour || hour === '') newErrors.hour = 'Hora inválida.'
   if (!regexMaximum60.test(place)) newErrors.place = 'Solo se permiten un máximo 60 de caracteres.'
   if (!place || place === '') newErrors.place = 'Ingresa un lugar.'
