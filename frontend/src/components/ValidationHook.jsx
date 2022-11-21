d2344cdea98b617c82ca053753b9fc1cf44b542a
dabonilla <dabonilla@unal.edu.co>
2022-11-21 14:00:34 -0500

    Añadida imagen por defecto en publicación de recomedaciones.

--- frontend/src/components/ValidationHook.jsx
+++ frontend/src/components/ValidationHook.jsx
@@ -51,14 +51,14 @@ const useValidationHook = (formName, url, reloadPage, closeModal) => {
     const formErrors = validateForm()
     if (Object.keys(formErrors).length > 0) {
       setErrors(formErrors)
-
     } else {
-
       closeModal();
-      uploadFile(file).then(async (downloadURL) => {
-        const newPublication = { ...inputs, image: downloadURL };
-        await publicRequest.post(url, newPublication, { withCredentials: true })
-        const Toast = Swal.mixin({
+      if (file == null){
+        const image = "https://firebasestorage.googleapis.com/v0/b/quienpaleer-c0891.appspot.com/o/1668972294675recommendation3.jpg?alt=media&token=c34d0221-8337-4636-bca0-a0a05652ab87"
+        async function publicationPost(){
+          const newPublication = { ...inputs, image: image };
+          await publicRequest.post(url, newPublication, { withCredentials: true })
+          const Toast = Swal.mixin({
           toast: true,
           position: 'top-end',
           showConfirmButton: false,
@@ -79,9 +79,38 @@ const useValidationHook = (formName, url, reloadPage, closeModal) => {
         setInputs({})
         setErrors({ file: null })
         setFile(null)
-      }).catch((error) => {
-        console.log(error);
-      })
+        }
+        publicationPost()
+      }
+      else{
+        uploadFile(file).then(async (downloadURL) => {
+          const newPublication = { ...inputs, image: downloadURL };
+          await publicRequest.post(url, newPublication, { withCredentials: true })
+          const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 2000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.addEventListener('mouseenter', Swal.stopTimer)
+              toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+          })
+          Toast.fire({
+            icon: 'success',
+            title: 'Publicación agregada.'
+          })
+          document.getElementById('publicationForm').reset()
+          reloadPage();
+          setPublication({})
+          setInputs({})
+          setErrors({ file: null })
+          setFile(null)
+        }).catch((error) => {
+          console.log(error);
+        })
+      }
     }
   }
 
@@ -89,7 +118,7 @@ const useValidationHook = (formName, url, reloadPage, closeModal) => {
     const { name, title, author, editorial, year, price, recommendation, summary, description, interest, startDate, endDate, hour, place } = inputs
     if (formName == "donation") return validateDonation(name, title, author, file, editorial)
     if (formName == "booksale") return validateBookSale(name, title, author, file, editorial, year, price)
-    if ( formName == "recommendation")return validateRecommendation(name, title, author, file, recommendation, summary)  
+    if ( formName == "recommendation")return validateRecommendation(name, title, author, "file", recommendation, summary)  
     if ( formName == "swap") return validateSwap(name, author, file, description, interest)
     if (formName == "event")return validateEvent(name, file, description, startDate, endDate, hour, place)
   }
